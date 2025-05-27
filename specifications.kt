c8900b072d4d266927ebe8b618e37869635c928e
Henrique Ignacio Ferreira Souza <henriquef621@gmail.com>
2025-05-27 10:41:48 -0300

    Add files via upload

---
+++ specifications.kt
@@ -0,0 +1,20 @@
+        As especificações do projeto são:
+
+        Objetivos
+    O objetivo é aprimorar meus conhecimentos em Kotlin.
+    Adiquirir conhecimento.
+    Aprender a usar novas ferramentas tools.
+
+        Instalação
+
+    1. Copie o repositorio.
+    2. Instale no seu Android Studio.
+
+        Tecnologia
+
+    A seguir as ferramentas que usei para o projeto
+    - IDE: Android Studio - https://developer.android.com/studio/install?hl=pt-br
+
+        Autor 
+
+    Developed by: Henrique Ignacio - https://www.linkedin.com/in/henrique-ignacio-ferreira-souza-6517562b9/
